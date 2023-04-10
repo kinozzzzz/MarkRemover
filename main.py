a88b00951e00a15916480a418890fe920cb1a53f
kkk <lxy2833704418@163.com>
2023-04-10 13:24:10 +0800

    bugfix

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 from pypdf import PdfReader,PdfWriter
 import sys
+import re
+
 def remove_link(page):
     for annot in page["/Annots"]:
         obj = annot.get_object()
         if obj["/Subtype"] == "/Link" and obj['/Private'] == "/UPDF":
+            print('delete link')
             page["/Annots"].remove(annot)
 
-def remove_updf(page):
-    if page["/Resources"]['/XObject'].get('/UPDFX1'):
-        del page["/Resources"]['/XObject']['/UPDFX1']
+def remove_resources(page):
+    resources = page['/Resources']
+    
+    def remove(k):
+        remove_key = []
+        for key in resources[k]:
+            if re.search("/UPDF",key):
+                remove_key.append(key)
+                print('delete mark')
+        for key in remove_key:
+            del resources[k][key]
+
+    remove('/XObject')
+    remove('/ExtGState')
 
 file = sys.argv[1]
 reader = PdfReader(file)
 writer = PdfWriter()
 for page in reader.pages:
     remove_link(page)
-    remove_updf(page)
+    remove_resources(page)
     writer.add_page(page)
 
 with open(file,"wb") as f:
